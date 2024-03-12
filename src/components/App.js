71e31bd5a3b9df4b6b8bb629b20313100c8c013b
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-03-12 06:32:40 +0000

    corrected

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import './../styles/App.css';
-import Greet from "./Greeting";
+import Greet from "./Greet";
 const App = () => {
   return (
     <div>
---
+++ src/components/Greet.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class Greet extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+  }
+
+  render() {
+    const handleChange = (e) => {
+      let val = e.target.value;
+      this.setState({ value: val });
+    };
+    return (
+      <>
+        <p>Enter your name:</p>
+        <input onChange={handleChange} type="text" />
+        {this.state.value ? <p>Hello {this.state.value}!</p> : null}
+      </>
+    );
+  }
+}
+
+export default Greet;
